169b196e0ec59f9406f1fbd67b4bad0cf5801db2
Emi-con <2398325@cegeplimoilou.ca>
2025-04-29 11:57:26 -0400

    utilisation class utils.py

--- ImageClassifier/api/app.py
+++ ImageClassifier/api/app.py
@@ -1,27 +1,36 @@
-from fastapi import FastAPI, UploadFile, File
-from tensorflow.keras.models import load_model
-from PIL import Image
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from tensorflow import keras
+from .utils import preprocess_image
+
 import numpy as np
-import io
 
 app = FastAPI()
-model = load_model("model/model.h5")
 
-class_names = ['avion', 'automobile', 'oiseau', 'chat', 'cerf',
-               'chien', 'grenouille', 'cheval', 'bateau', 'camion']
+#Charger le modèle
+model = keras.models.load_model('model/model.h5')
+
+#Définir les classes CIFAR-10
+noms_classes = ['avion', 'automobile', 'oiseau', 'chat', 'cerf', 'chien', 'grenouille', 'cheval', 'bateau', 'camion']
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents)).resize((32, 32)).convert("RGB") #Lit le contenu binaire de l’image envoyée.
-    image_array = np.expand_dims(np.array(image) / 255.0, axis=0)
+    #Lire l'image
+    image_bytes = await file.read()
 
-    prediction = model.predict(image_array)[0] 
-    predicted_class = class_names[np.argmax(prediction)]
-# Associer chaque classe à son pourcentage
-    prediction_percentages = {class_name: float(f"{prob*100:.2f}") for class_name, prob in zip(class_names, prediction)}
+    #Prétraiter l'image
+    image = preprocess_image(image_bytes)
+    
+    #Faire la prédiction
+    prediction = model.predict(image)[0]
+
+    # Classe prédite
+    predicted_class = noms_classes[np.argmax(prediction)]
+
+    # Probabilités arrondies
+    prediction_percentages = {class_name: float(f"{prob*100:.2f}") for class_name, prob in zip(noms_classes, prediction)}
 
     return {
-        "prediction": predicted_class,
-        "probabilities": prediction_percentages
-    }
+            "prediction": predicted_class,
+            "probabilities": prediction_percentages
+        }
